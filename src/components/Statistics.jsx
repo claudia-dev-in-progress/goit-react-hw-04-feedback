69f8f81874ddac3df027186bea34a4f81b067104
claudiapapp <pappileanaclaudia@gmail.com>
2023-09-13 22:57:10 +0300

    added initial solution

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Statistics extends Component {
+  static propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
+  };
+
+  render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive percentage: {positivePercentage}%</p>
+      </>
+    );
+  }
+}
